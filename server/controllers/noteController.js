13ed02ebabee650876158655c842062b9d94eaf4
Tiago Mendes <tetigo@gmail.com>
2025-08-22 17:54:53 -0300

    fix: add missing console log required in the document

--- server/controllers/noteController.js
+++ server/controllers/noteController.js
@@ -2,6 +2,7 @@ const service = require("../models/Note");
 
 export const getAllNotes = (req, res) => {
   const notes = service.getAllNotesService();
+  console.log("GET /notes - All notes:", notes);
   res.json(notes);
 };
 
@@ -9,6 +10,7 @@ export const getNoteById = (req, res) => {
   const id = parseInt(req.params.id);
   const note = service.getNoteByIdService(id);
   if (!note) return res.status(404).json({ error: "Note not found" });
+  console.log(`GET /notes/${req.params.id} - Note:`, note);
   res.json(note);
 };
 
@@ -16,6 +18,7 @@ export const createNote = (req, res) => {
   const { title, content } = req.body;
   if (!title) return res.status(400).json({ error: "Title is required" });
   const note = service.createNoteService({ title, content });
+  console.log("POST /notes - Created note:", note); // 
   res.status(201).json(note);
 };
 
@@ -24,6 +27,7 @@ export const updateNote = (req, res) => {
   const { title, content } = req.body;
   const note = service.updateNoteService(id, { title, content });
   if (!note) return res.status(404).json({ error: "Note not found" });
+  console.log(`PUT /notes/${req.params.id} - Updated note:`, note);
   res.json(note);
 };
 
@@ -31,5 +35,6 @@ export const deleteNote = (req, res) => {
   const id = parseInt(req.params.id);
   const success = service.deleteNoteService(id);
   if (!success) return res.status(404).json({ error: "Note not found" });
+  console.log(`DELETE /notes/${req.params.id} - Deleted?`, success);
   res.json({ message: "Note deleted successfully" });
 };
